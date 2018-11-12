d142d96b861bf78c8ae492d080bcc1314e160b67
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2018-11-12 18:12:23 +0000

    Add description of SRP Checkout Kata exercise and extra tests

--- src/test/java/checkout/CheckoutTests.java
+++ src/test/java/checkout/CheckoutTests.java
@@ -7,6 +7,75 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CheckoutTests {
 
     @Test
+    public void oneA() {
+        Checkout checkout = new Checkout();
+
+        checkout.scan("A");
+        assertThat(checkout.total()).isEqualTo(50);
+    }
+
+    @Test
+    public void twoA() {
+        Checkout checkout = new Checkout();
+
+        checkout.scan("A");
+        checkout.scan("A");
+        assertThat(checkout.total()).isEqualTo(100);
+    }
+
+    @Test
+    public void threeA() {
+        Checkout checkout = new Checkout();
+
+        checkout.scan("A");
+        checkout.scan("A");
+        checkout.scan("A");
+        assertThat(checkout.total()).isEqualTo(130);
+    }
+
+
+    @Test
+    public void sixA() {
+        Checkout checkout = new Checkout();
+
+        checkout.scan("A");
+        checkout.scan("A");
+        checkout.scan("A");
+        checkout.scan("A");
+        checkout.scan("A");
+        checkout.scan("A");
+        assertThat(checkout.total()).isEqualTo(260);
+    }
+
+    @Test
+    public void oneB() {
+        Checkout checkout = new Checkout();
+
+        checkout.scan("B");
+        assertThat(checkout.total()).isEqualTo(30);
+    }
+
+    @Test
+    public void twoB() {
+        Checkout checkout = new Checkout();
+
+        checkout.scan("B");
+        checkout.scan("B");
+        assertThat(checkout.total()).isEqualTo(45);
+    }
+
+    @Test
+    public void fourB() {
+        Checkout checkout = new Checkout();
+
+        checkout.scan("B");
+        checkout.scan("B");
+        checkout.scan("B");
+        checkout.scan("B");
+        assertThat(checkout.total()).isEqualTo(90);
+    }
+
+    @Test
     public void simple() {
         Checkout checkout = new Checkout();
         
