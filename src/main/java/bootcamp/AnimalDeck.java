863712973c7a6e6930602d51c37f85ea5758cfc0
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2018-11-13 11:32:57 +0000

    Filling out content for day 1 - added missing AnimalDeck class

---
+++ src/main/java/bootcamp/AnimalDeck.java
@@ -0,0 +1,41 @@
+package bootcamp;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class AnimalDeck {
+
+    private List<AnimalCard> cards;
+
+    AnimalDeck() {
+        cards = new ArrayList<>();
+        for (Animal animal : Animal.values()) {
+            cards.add(new AnimalCard(animal));
+            cards.add(new AnimalCard(animal));
+        }
+    }
+
+    String[] shuffle() {
+        String[] result = new String[cards.size()];
+        Collections.shuffle(cards);
+        for (int i = 0; i < cards.size(); i++) {
+            AnimalCard card = cards.get(i);
+            result[i] = card.toString();
+        }
+        return result;
+    }
+
+    String[] getCards() {
+        String[] result = new String[cards.size()];
+        for (int i = 0; i < cards.size(); i++) {
+            AnimalCard card = cards.get(i);
+            result[i] = card.toString();
+        }
+        return result;
+    }
+
+    AnimalCard deal() {
+        return cards.remove(0);
+    }
+}
