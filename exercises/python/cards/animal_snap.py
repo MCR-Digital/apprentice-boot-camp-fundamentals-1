77307993ec5396f7ebb66d3d3808f12789375737
Caroline Hatwell <caroline.hatwell@coopdigital.co.uk>
2018-11-19 16:40:30 +0000

    added animal snap class to differentiate from regular card snap

---
+++ exercises/python/cards/animal_snap.py
@@ -0,0 +1,52 @@
+from animal_deck import AnimalDeck
+
+
+class Snap:
+
+    def __init__(self, deck):
+        self.player_1_score = 0
+        self.player_2_score = 0
+        self.deck = deck
+        self.deck.shuffle()
+
+    def play(self):
+        previous_card = None
+        while len(self.deck.cards) > 0:
+            current_card = self.deck.deal()
+            print(current_card.animal)
+            player_input = raw_input("Your turn: ")
+            print("PLAYER INPUT: ", player_input)
+            if len(player_input) > 0 and player_input[0] == 'a':
+                if current_card.snap(previous_card):
+                    print("SNAP! score Player 1")
+                    self.player_1_score += 1
+                else:
+                    print("WRONG! deducting score from Player 1")
+                    self.player_1_score -= 1
+            elif len(player_input) > 0 and player_input[0] == 'l':
+                if current_card.snap(previous_card):
+                    print("SNAP! scorePlayer 2")
+                    self.player_2_score += 1
+                else:
+                    print("WRONG! deducting score from Player 2")
+                    self.player_2_score -= 1
+
+            previous_card = current_card
+
+        if self.player_1_score == self.player_2_score:
+            print("Draw")
+        elif self.player_1_score > self.player_2_score:
+            print("Player 1 wins!")
+        else:
+            print("Player 2 wins!")
+
+        print("Scores: {player_1_score} vs {player_2_score}".format(
+            player_1_score=self.player_1_score, player_2_score=self.player_2_score))
+
+
+def play_snap():
+    snap = Snap(AnimalDeck())
+    snap.play()
+
+
+play_snap()
