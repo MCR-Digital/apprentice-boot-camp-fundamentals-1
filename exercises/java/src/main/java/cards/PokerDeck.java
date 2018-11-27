5329be17a30d37d534731248be3d3e07a7634370
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2018-11-27 16:25:36 +0000

    Convert FaceValue enum to a class
    This removes dependence on switch ordering and enum value ordering.

--- exercises/java/src/main/java/cards/PokerDeck.java
+++ exercises/java/src/main/java/cards/PokerDeck.java
@@ -1,23 +1,32 @@
 package cards;
 
-import java.util.ArrayList;
 import java.util.List;
 
 class PokerDeck extends Deck {
 
     PokerDeck() {
-        cards = new ArrayList<>();
-        for (Suit suit: Suit.values()) {
-            for (FaceValue faceValue : FaceValue.values()) {
-                cards.add(new PokerCard(suit, faceValue));
-            }
+        super();
+        for (Suit suit : Suit.values()) {
+            cards.add(new PokerCard(suit, new FaceValue("ace")));
+            cards.add(new PokerCard(suit, new FaceValue("2")));
+            cards.add(new PokerCard(suit, new FaceValue("3")));
+            cards.add(new PokerCard(suit, new FaceValue("4")));
+            cards.add(new PokerCard(suit, new FaceValue("5")));
+            cards.add(new PokerCard(suit, new FaceValue("6")));
+            cards.add(new PokerCard(suit, new FaceValue("7")));
+            cards.add(new PokerCard(suit, new FaceValue("8")));
+            cards.add(new PokerCard(suit, new FaceValue("9")));
+            cards.add(new PokerCard(suit, new FaceValue("10")));
+            cards.add(new PokerCard(suit, new FaceValue("jack")));
+            cards.add(new PokerCard(suit, new FaceValue("queen")));
+            cards.add(new PokerCard(suit, new FaceValue("king")));
         }
     }
 
     public static void main(String[] args) {
         PokerDeck pokerDeck = new PokerDeck();
         List<Card> deckInOrder = pokerDeck.getCards();
-        for(Card card: deckInOrder){
+        for (Card card : deckInOrder) {
             System.out.println(card);
         }
     }
