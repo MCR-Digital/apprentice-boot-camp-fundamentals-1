18af6e8da59a83eacb45a9e91421c03fc5fcc880
Ruben Wolff <ruben.wolff@zuto.com>
2019-11-19 15:39:43 +0000

    Cleanup Checkout code to make it easier to instantly use in VS
    - added solution file
    - used auto format
    - removed uses of this

--- exercises/dotnet/McrDigital.Bootcamp1.Checkout/Checkout.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/Checkout.cs
@@ -1,44 +1,61 @@
-namespace McrDigital.Bootcamp1.Checkout {
-  public class Checkout {
-    private int _total;
-    private int _numberOfA;
-    private int _numberOfB;
-    private Receipt _receipt = new Receipt();
+namespace McrDigital.Bootcamp1.Checkout
+{
+    public class Checkout
+    {
+        private int _total;
+        private int _numberOfA;
+        private int _numberOfB;
+        private Receipt _receipt = new Receipt();
 
-    public string Receipt {
-      get => this._receipt.Text;
-    }
-
-    public int Total {
-      get => this._total;
-    }
-
-    public void Scan(string sku) {
-      if ("A".Equals(sku)) {
-        this._total += 50;
-        this._receipt.ScannedA();
-      } else if ("B".Equals(sku)) {
-        this._total += 30;
-        this._receipt.ScannedB();
-      } else if ("C".Equals(sku)) {
-        this._total += 20;
-        this._receipt.ScannedC();
-      } else if ("D".Equals(sku)) {
-        this._total += 15;
-        this._receipt.ScannedD();
-      }
-
-      if ("A".Equals(sku)) {
-        this._numberOfA++;
-        if (this._numberOfA % 3 == 0) {
-          this._total -= 20;
+        public string Receipt
+        {
+            get => _receipt.Text;
         }
-      } else if ("B".Equals(sku)) {
-        this._numberOfB++;
-        if (this._numberOfB % 2 == 0) {
-          this._total -= 15;
+
+        public int Total
+        {
+            get => _total;
         }
-      }
+
+        public void Scan(string sku)
+        {
+            if ("A".Equals(sku))
+            {
+                _total += 50;
+                _receipt.ScannedA();
+            }
+            else if ("B".Equals(sku))
+            {
+                _total += 30;
+                _receipt.ScannedB();
+            }
+            else if ("C".Equals(sku))
+            {
+                _total += 20;
+                _receipt.ScannedC();
+            }
+            else if ("D".Equals(sku))
+            {
+                _total += 15;
+                _receipt.ScannedD();
+            }
+
+            if ("A".Equals(sku))
+            {
+                _numberOfA++;
+                if (_numberOfA % 3 == 0)
+                {
+                    _total -= 20;
+                }
+            }
+            else if ("B".Equals(sku))
+            {
+                _numberOfB++;
+                if (_numberOfB % 2 == 0)
+                {
+                    _total -= 15;
+                }
+            }
+        }
     }
-  }
 }
