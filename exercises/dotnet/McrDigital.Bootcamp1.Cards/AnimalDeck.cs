d232f18106cbf9ca714ad4ac8c27f94a405c040e
Ruben Wolff <ruben.wolff@zuto.com>
2019-11-19 15:37:51 +0000

    Some cleanup to make it easier to open the dotnet project in VS and start coding
    - Adding gitignore for .net specifc code
    - Auto formatted spacing so that adding code in VS does not mess up the spacing
    - Removed redundant uses of this
    - Moved ListExtensions into separate file (it will be reused in other Deck implementations)
    - added solution file
    - made Cards project a console app entering at Snap.cs so the project can be debugged immediately

--- exercises/dotnet/McrDigital.Bootcamp1.Cards/AnimalDeck.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Cards/AnimalDeck.cs
@@ -1,49 +1,44 @@
-namespace McrDigital.Bootcamp1.Cards {
-  using System;
-  using System.Collections.Generic;
+namespace McrDigital.Bootcamp1.Cards
+{
+    using System;
+    using System.Collections.Generic;
 
-  public class AnimalDeck {
-    private readonly List<AnimalCard> _cards;
+    public class AnimalDeck
+    {
+        private readonly List<AnimalCard> _cards;
 
-    public AnimalDeck() {
-      this._cards = new List<AnimalCard>();
-      foreach (Animal animal in Enum.GetValues(typeof(Animal))) {
-        this._cards.Add(new AnimalCard(animal));
-        this._cards.Add(new AnimalCard(animal));
-      }
+        public AnimalDeck()
+        {
+            _cards = new List<AnimalCard>();
+            foreach (Animal animal in Enum.GetValues(typeof(Animal)))
+            {
+                _cards.Add(new AnimalCard(animal));
+                _cards.Add(new AnimalCard(animal));
+            }
+        }
+
+        public AnimalCard Deal()
+        {
+            var card = _cards[0];
+            _cards.RemoveAt(0);
+            return card;
+        }
+
+        public string[] GetCards()
+        {
+            var result = new string[_cards.Count];
+            for (var index = 0; index < _cards.Count; index++)
+            {
+                var card = _cards[index];
+                result[index] = card.ToString();
+            }
+
+            return result;
+        }
+
+        public void Shuffle()
+        {
+            _cards.KnuthShuffle();
+        }
     }
-
-    public AnimalCard Deal() {
-      var card = this._cards[0];
-      this._cards.RemoveAt(0);
-      return card;
-    }
-
-    public string[] GetCards() {
-      var result = new String[this._cards.Count];
-      for (var index = 0; index < this._cards.Count; index++) {
-        var card = this._cards[index]; result[index] = card.ToString();
-      }
-      return result;
-    }
-
-    public void Shuffle() {
-      this._cards.KnuthShuffle();
-    }
-  }
-
-  public static class ListExtensions {
-    public static void KnuthShuffle<T>(this List<T> @this) {
-      // Use the Knuth Shuffle (aka Fisher-Yates shuffle) to shuffle the list of cards.
-      // https://en.wikipedia.org/wiki/Fisher–Yates_shuffle
-      // https://stackoverflow.com/a/25943363
-      var random = new Random();
-      for (var index = (@this.Count - 1); index > 0; index--) {
-        var next = random.Next(index + 1);
-        var temp = @this[index];
-        @this[index] = @this[next];
-        @this[next] = temp;
-      }
-    }
-  }
 }
