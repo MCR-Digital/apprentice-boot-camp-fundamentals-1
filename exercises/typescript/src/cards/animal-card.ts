f0d3dca0af235583091cebe9e11700d451b8fb5a
Dru Chavda <21034587+drulmc@users.noreply.github.com>
2022-11-11 17:15:24 +0000

    convert cards to ts, add run scripts

--- exercises/typescript/src/cards/animal-card.ts
+++ exercises/typescript/src/cards/animal-card.ts
@@ -1,15 +1,15 @@
 export class AnimalCard {
-  private animal: string
+  private readonly animal: string
 
   constructor(animal: string) {
     this.animal = animal
   }
 
-  snap(otherCard: AnimalCard) {
+  snap(otherCard: AnimalCard): boolean {
     return otherCard && this.animal === otherCard.animal
   }
 
-  toString() {
+  toString(): string {
     return this.animal.toString()
   }
 }
