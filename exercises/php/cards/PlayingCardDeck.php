612cbe8a7e4ae2cffafb348aa50c83b4122f9e97
Sam Watkinson <samwatkinson@outlook.com>
2022-11-17 18:19:04 +0000

    refactor(Deck): change to be an interface instead of an abstract class

--- exercises/php/cards/PlayingCardDeck.php
+++ exercises/php/cards/PlayingCardDeck.php
@@ -2,8 +2,10 @@
 
 namespace McrDigital\PhpFundamentals1\Cards;
 
-class PlayingCardDeck extends Deck
+class PlayingCardDeck implements Deck
 {
+    private array $cards;
+
     public function __construct()
     {
         $cSuits = new Suit();
@@ -13,6 +15,35 @@ class PlayingCardDeck extends Deck
             for ($faceValue = 0; $faceValue < 13; $faceValue++) {
                 $this->cards[] = new PlayingCard($suit, $faceValue);
             }
+        }
+    }
+
+    public function getCards(): array
+    {
+        $result = [];
+        foreach ($this->cards as $card) {
+            $result[] = strval($card);
+        }
+        return $result;
+    }
+
+    public function deal(): Card
+    {
+        $card = array_splice($this->cards, 0, 1)[0];
+        return $card;
+    }
+
+    public function shuffle(): void
+    {
+        for ($i = 0; $i < count($this->cards); ++$i) {
+            $indexA = rand(0, $i);
+            $indexB = $i;
+
+            $valueA = $this->cards[$indexA];
+            $valueB = $this->cards[$indexB];
+
+            $this->cards[$indexA] = $valueB;
+            $this->cards[$indexB] = $valueA;
         }
     }
 
