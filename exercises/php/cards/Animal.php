2df95b2e0423866bf9c4735443b120b0ba852f5c
Sam Watkinson <41587235+samwatkinson1@users.noreply.github.com>
2022-11-17 16:29:50 +0000

    feat: Convert master branch exercises to PHP (#123)
    * chore(exercises): scaffold php directory + deps
    
    * chore: remove adhocore/cli and implement platform requirements and namespace support
    
    * refactor: remove index.php
    
    * chore: require ext-readline
    
    * feat: implement snap game in PHP
    
    * chore: update README.md with setup instructions
    
    * chore: add phpunit to dev dependencies
    
    * feat: add test for cards/CAnimalDeck.php
    
    * feat: add cards.php and test
    
    * fix: relative import for non-namespaced class
    
    * refactor: rename tests to jest-like descriptors
    
    - e.g. 'should handle', 'should return' etc.
    
    * refactor: cas iterated card as string when calling CAnimalDeck::getCards()
    
    * refactor: rename Cards to CCards
    
    * refactor: simplify CAnimal::animal array to be an indexed instead of associative array
    
    * refactor: remove C* prefix from classes
    
    * feat: implement checkout/
    
    (cherry picked from commit e1e0ef786fdae4147d2ecebed3cc478e42b3cf50)

---
+++ exercises/php/cards/Animal.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace McrDigital\PhpFundamentals1\Cards;
+
+class Animal
+{
+    public array $animal = [
+        "AARDVARK",
+        "BABOON",
+        "CAMEL",
+        "DEER",
+        "ELEPHANT",
+        "FROG",
+        "GORILLA",
+        "HARE",
+        "IMPALA",
+        "JAGUAR",
+        "KANGAROO",
+        "LION",
+        "MOOSE",
+        "NEWT",
+        "OCTOPUS",
+        "PENGUIN",
+        "QUETZAL",
+        "RABBIT",
+        "SALMON",
+        "TORTOISE",
+        "UAKARIS",
+        "VAQUITA",
+        "WHALE",
+        "X_RAY_TETRA",
+        "YAK",
+        "ZEBRA"
+    ];
+
+}
