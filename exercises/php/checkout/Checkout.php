a4326a3a45802674cf2eb814f32c9c7e857b2ed9
Sam Watkinson <samwatkinson@outlook.com>
2022-11-17 16:06:07 +0000

    feat: implement checkout/

---
+++ exercises/php/checkout/Checkout.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace McrDigital\PhpFundamentals1\Checkout;
+
+class Checkout
+{
+    private int $total;
+    private int $numberOfA;
+    private int $numberOfB;
+    private Receipt $receipt;
+
+    public function __construct()
+    {
+        $this->total = 0;
+        $this->numberOfA = 0;
+        $this->numberOfB = 0;
+        $this->receipt = new Receipt();
+    }
+
+    public function scan(string $sku)
+    {
+        if ('A' === $sku) {
+            $this->total += 50;
+            $this->receipt->scannedA();
+        } else if ('B' === $sku) {
+            $this->total += 30;
+            $this->receipt->scannedB();
+        } else if ('C' === $sku) {
+            $this->total += 20;
+            $this->receipt->scannedC();
+        } else if ('D' == $sku) {
+            $this->total += 15;
+            $this->receipt->scannedD();
+        }
+
+        if ('A' == $sku) {
+            $this->numberOfA++;
+            if ($this->numberOfA % 3 === 0) {
+                $this->total -= 20;
+            }
+        } else if ('B' == $sku) {
+            $this->numberOfB++;
+            if ($this->numberOfB % 2 === 0) {
+                $this->total -= 15;
+            }
+        }
+    }
+
+    public function getTotal(): int
+    {
+        return $this->total;
+    }
+
+    public function getReceipt(): string
+    {
+        return $this->receipt->getText();
+    }
+
+}
