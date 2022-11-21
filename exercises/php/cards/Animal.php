18f0bbe2d5b5a45aa0c39caa1fb98fdccfabab3b
Sam Watkinson <samwatkinson@outlook.com>
2022-11-21 17:37:04 +0000

    refactor: extract ANIMALS const and type animal values as instance of Animal class

--- exercises/php/cards/Animal.php
+++ exercises/php/cards/Animal.php
@@ -4,33 +4,17 @@ namespace McrDigital\PhpFundamentals1\Cards;
 
 class Animal
 {
-    public array $animal = [
-        "AARDVARK",
-        "BABOON",
-        "CAMEL",
-        "DEER",
-        "ELEPHANT",
-        "FROG",
-        "GORILLA",
-        "HARE",
-        "IMPALA",
-        "JAGUAR",
-        "KANGAROO",
-        "LION",
-        "MOOSE",
-        "NEWT",
-        "OCTOPUS",
-        "PENGUIN",
-        "QUETZAL",
-        "RABBIT",
-        "SALMON",
-        "TORTOISE",
-        "UAKARIS",
-        "VAQUITA",
-        "WHALE",
-        "X_RAY_TETRA",
-        "YAK",
-        "ZEBRA"
-    ];
+    private string $animalName;
+
+    public function __construct(string $animalName)
+    {
+        $this->animalName = $animalName;
+    }
+
+    public function __toString(): string
+    {
+        return $this->animalName;
+    }
+
 
 }
